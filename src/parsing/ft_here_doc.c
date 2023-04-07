79618735f352d169fbe81d991ad2a1c8c43e7aae
Hsin-yu Liu <hsliu@z4r10p1.42lyon.fr>
2023-04-07 14:39:32 +0200

    heredoc \n

--- src/parsing/ft_here_doc.c
+++ src/parsing/ft_here_doc.c
@@ -6,7 +6,7 @@
 /*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 10:43:28 by hsliu             #+#    #+#             */
-/*   Updated: 2023/04/07 14:38:43 by hsliu            ###   ########.fr       */
+/*   Updated: 2023/04/07 14:39:27 by hsliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ void	ft_child_here_doc(char *limiter, int *fd)
 			return ;
 		}
 		write(fd[1], input, ft_strlen(input));
-		write(fd[1], '\n', 1);
+		write(fd[1], "\n", 1);
 		free(input);
 	}
 }
